7c5a958293f147a245a42ddfd0430c525638c7e1
Nepochtov Dmitry <dimendroider@gmail.com>
2025-10-19 03:33:54 +0300

    react components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="title">Header</h1>
+      <p className="text">Text Lorem lorem</p>
+      <h2 className="subtitle">Footer</h2>
+    </>
+  )
+}
+
+
+export default App;
---
+++ src/Module.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Module = () => {
+  return (
+    <>
+      <h2>Привет React</h2>
+    </>
+  )
+}
+
+
+export default Module;
