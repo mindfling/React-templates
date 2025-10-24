a5916651b503653e915ae3355ddf4723dfcc2a19
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2025-10-24 20:47:39 +0300

    Layout container

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,12 @@
+// * Layout Container
 import style from './Layout.module.css';
 
 export const Layout = ({props}) => {
-  console.log('Container');
+  console.log('Container', style);
   
   return (
     <>
-      <div className="container">
+      <div className={style.container}>
         {props.children}
       </div>
     </>
