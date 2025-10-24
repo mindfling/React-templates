6b69739eda7cdc134dc0fbd650d6d2de032ab0ab
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2025-10-24 23:52:09 +0300

    Heading headling title

---
+++ src/components/Headling/Headling.jsx
@@ -0,0 +1,12 @@
+import style from './Heading.module.css';
+
+export const Heading = ({ text }) => {
+  
+  return (
+    <>
+      <h1 className={style.heading}>
+        {text}
+      </h1>
+    </>
+  )
+}
