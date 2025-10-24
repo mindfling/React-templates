a8720e37d26653cd05857db59b7cb7e6831067e5
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2025-10-24 23:52:22 +0300

    Container

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+// * Layout Container
+import style from './Layout.module.css';
+
+export const Layout = ( props ) => {
+  console.log('Container', style);
+  
+  return (
+    <>
+      <div className={style.container}>
+        {props.children}
+      </div>
+    </>
+  )
+}
