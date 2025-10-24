d7c3bc21e4d90a679b613b6d508e767968ef0b14
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2025-10-24 20:00:31 +0300

    Main

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import style from './Main.module.css';
+
+export const Main = () => {
+  
+  return (
+    <>
+      <div className={style.Main}>
+        <div className="container">
+          
+        </div>
+      </div>
+    </>
+  )
+}
