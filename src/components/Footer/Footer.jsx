10cd6a5883d6162193a79cb538d7507ad1943fa0
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2025-10-24 20:00:42 +0300

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import style from './Footer.module.css';
+
+export const Footer = () => {
+  
+  return (
+    <>
+      <div className={style.footer}>
+        <div className="container">
+          
+        </div>
+      </div>
+    </>
+  )
+}
