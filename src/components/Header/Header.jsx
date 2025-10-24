d425e9f387095a937241652b650922ecb9f0a259
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2025-10-24 23:00:24 +0300

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import style from './Header.module.css';
+import Layout from '../Layout';
+import Logo from '../Logo';
+// import LogoSvg from '../LogoSvg';
+import Headling from '../Headling';
+import Search from '../Search';
+import Auth from '../Auth';
+
+
+export const Header = () => {
+  console.log('Header');
+
+  return (
+    <>
+      <header className={style.header}>
+        <Layout>
+          <div className={style.gridContainer}>
+            <Logo />
+            <Headling text='Blogget' />
+            <Search />
+            <Auth />
+          </div>
+        </Layout>
+      </header>
+    </>
+  );
+};
