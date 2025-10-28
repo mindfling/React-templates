37206e8900893780bc499c50d779a793d360a49c
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2025-10-28 14:33:00 +0300

    feat Auth svg

---
+++ src/components/Auth/AuthImg.jsx
@@ -0,0 +1,16 @@
+import style from './Auth.module.css';
+import loginImg from './img/login.svg';
+
+export const Auth = ({ auth }) => {
+  console.log('Auth IMG');
+
+  return (
+    <>
+      <div className={style.auth}>
+        <button className={style.button}>
+          <img className={style.svg} src={loginImg} alt="Авторизация пользователя Login user picture" />
+        </button>
+      </div>
+    </>
+  );
+};
