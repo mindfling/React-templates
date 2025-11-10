8ae46c970bb130b2b63f741342e72ffe9f73c853
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2025-11-10 21:48:43 +0300

    feat Post rating count up and down

--- src/components/Main/List/Post/PostRating/PostRating.jsx
+++ src/components/Main/List/Post/PostRating/PostRating.jsx
@@ -1,23 +1,47 @@
 /* eslint-disable */
+import { useState } from 'react';
 import style from './PostRating.module.css';
 import PropTypes from 'prop-types';
 
+export const PostRating = ({ ups }) => {
+  const [rating, setRating] = useState(ups);
+  console.log(`Functional PostRating loaded\ncurrent rating is ${rating}`);
 
-export const PostRating = ({ups}) => {
-  console.log('Functional PostRating loaded');
+  const handleInc = () => {
+    setRating((rating) => rating + 1);
+  };
+
+  const handleDec = () => {
+    if (rating <= 0) {
+      return console.log('Рейтин поста уже ниже нижнего');
+    }
+    setRating((rating) => rating - 1);
+  };
+
   return (
     <>
       <div className={style.rating}>
-        <button className={style.up} aria-label='Увеличить рейтинг поста' title='Увеличить рейтинг' />
-        <p className={style.ups} title={`Текущий рейтинг поста ${ups}`}>{ups}</p>
-        <button className={style.down} aria-label='Уменьшить рейтинг поста' title='Уменьшить рейтинг' />
+        <button
+          className={style.up}
+          aria-label="Увеличить рейтинг поста"
+          title="Увеличить рейтинг"
+          onClick={handleInc}
+        />
+        <p className={style.ups} title={`Текущий рейтинг поста ${ups}`}>
+          {rating}
+        </p>
+        <button
+          className={style.down}
+          aria-label="Уменьшить рейтинг поста"
+          title="Уменьшить рейтинг"
+          onClick={handleDec}
+        />
       </div>
     </>
   );
 };
 
-
 // props validation
 PostRating.propTypes = {
-  ups: PropTypes.string,
+  ups: PropTypes.number,
 };
