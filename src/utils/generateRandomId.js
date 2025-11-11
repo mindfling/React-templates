449bcb99920abd8bc10cb1b96e3ec653224d4703
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2025-11-12 00:19:05 +0300

    Update generateRandomId.js

--- src/utils/generateRandomId.js
+++ src/utils/generateRandomId.js
@@ -1,11 +1,30 @@
-// * функция генерации псевдоуникального идентефикатора
-const generateId = () => {
+
+export const getParseFileName = (uri) => {
+  const arr = uri.toString().split('\/');
+  return arr.at(-1);
+}
+
+
+export const capitalize = word => `${word.charAt(0).toUpperCase()}${word.substring(1)}`;
+
+
+export const generateId = () => {
   return Math.random().toString(36).substring(2, 10);
-}
+};
 
-console.log('    ', '12345678', 0);
-for(let i = 0; i <= 10; i++) {
-  console.log('test', generateId(), i);
-}
 
-export default generateId;
+export const generateRandomId = () => {
+  return `0x${Math.random().toString(36).substring(2, 6)}x${Date.now().toString().substring(9)}`;
+};
+
+
+export const assignId = (obj) => {
+  return {
+    ...obj,
+    id: generateRandomId(), 
+    // thumbnail: getParseFileName(obj.image),
+    // nickname: obj.author,
+    // author: obj.author.split(' ').map(word => capitalize(word)).join(' '),
+    // lang: (obj.lang === 'ru' ? 'ru' : 'eng'),
+  };
+};
