e6aa9f53badaeeb2c93351e7991856a66dacec9e
Nepochtov Dmitry <dimendroider@gmail.com>
2025-11-13 00:30:25 +0300

    fix some

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
 import { createRoot } from 'react-dom/client';
-// * by export default App
-import App from './App';
+import App from "./App";
 import 'normalize.css';
 import './index.css';
 
-const root = createRoot(document.querySelector('#root'));
-root.render(
+createRoot(document.querySelector('#root'))
+  .render(
     <App />
 );
--- src/utils/generateRandomId.js
+++ src/utils/generateRandomId.js
@@ -1,30 +1,21 @@
-
 export const getParseFileName = (uri) => {
   const arr = uri.toString().split('\/');
   return arr.at(-1);
 }
 
-
 export const capitalize = word => `${word.charAt(0).toUpperCase()}${word.substring(1)}`;
-
 
 export const generateId = () => {
   return Math.random().toString(36).substring(2, 10);
 };
 
-
 export const generateRandomId = () => {
   return `0x${Math.random().toString(36).substring(2, 6)}x${Date.now().toString().substring(9)}`;
 };
-
 
 export const assignId = (obj) => {
   return {
     ...obj,
     id: generateRandomId(), 
-    // thumbnail: getParseFileName(obj.image),
-    // nickname: obj.author,
-    // author: obj.author.split(' ').map(word => capitalize(word)).join(' '),
-    // lang: (obj.lang === 'ru' ? 'ru' : 'eng'),
   };
 };
