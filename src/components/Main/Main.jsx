3f6f7524c15119d90dae8c64eb384cd40ee8c0e4
Nepochtov Dmitry <dimendroider@gmail.com>
2025-11-13 19:38:55 +0300

    realize add todo USEIT

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,32 +8,29 @@ import { useState } from 'react';
 
 // список меню
 const LIST = [
-  {
-    value: 'Главная',
-    slug: 'home',
-  },
-  {
-    value: 'Просмотренные',
-    slug: 'visited',
-  },
-  {
-    value: 'Сохраненные',
-    slug: 'saved',
-  },
-  {
-    value: 'Мои посты',
-    slug: 'myposts',
-  },
+  { value: 'Главная', },
+  { value: 'Просмотренные', },
+  { value: 'Сохраненные', },
+  { value: 'Мои посты', },
 ].map(assignId);
 
 
 export const Main = () => {
   const [list, setList] = useState(LIST);
   console.log('list in Main: ', list);
+
+  const addItem = () => {
+    // todo it can be used in todo!!!
+    console.log('add item');
+    // setList(list => list.concat(assignId({value: 'New item'}))); // или так
+    setList((list) => [...list, assignId(assignId({ value: "New item" }))]); // так или
+  }
+
   return (
     <>
       <main className={style.main}>
         <Layout>
+          <button className="addItem" onClick={addItem}>Кнопка добавить todo Item</button>
           <Tabs list={list} setList={setList} />
           {/* <List /> */}
         </Layout>
--- src/components/Main/Tabs/Tabs.jsx
+++ src/components/Main/Tabs/Tabs.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 
 export const Tabs = ({ list, setList }) => {
   const handleClick = (id) => {
-    console.log('id in handleClick: ', id);
+    console.log('del id in handleClick: ', id);
     setList(list.filter(item => item.id !== id));
   }
   
@@ -12,9 +12,10 @@ export const Tabs = ({ list, setList }) => {
     <ul className={style.list}>
       {list.map((tab) => (
         <li className={style.item} key={tab.id}>
-          <button className={style.btn} id={tab.id} onClick={(e) => {
-            handleClick(e.target.id)
-          }}>
+          <button
+            className={style.btn}
+            onClick={() => handleClick(tab.id)}
+          >
             {tab.value}
           </button>
         </li>
@@ -23,7 +24,9 @@ export const Tabs = ({ list, setList }) => {
   );
 };
 
+
 Tabs.propTypes = {
   list: PropTypes.array,
+  setList: PropTypes.func,
   tabs: PropTypes.array,
 };
